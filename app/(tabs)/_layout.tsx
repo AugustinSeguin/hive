7a0f0787b002cd148666ee6e8b0db5b0df7d9614
nejmalj <nejmalejeannic@gmail.com>
2025-09-10 16:48:52 +0200

    Navbar + dependencies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,31 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    return (
+        <Tabs
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+                    if (route.name === "index") {
+                        iconName = "list-outline";
+                    } else if (route.name === "ranking") {
+                        iconName = "trophy-outline";
+                    } else if (route.name === "household") {
+                        iconName = "home-outline";
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: "#1d4ed8", // à modifier quand on aura la maquette finale
+                tabBarInactiveTintColor: "gray", // à modifier quand on aura la maquette finale
+                headerShown: false,
+            })}
+        >
+            <Tabs.Screen name="index" options={{ title: "Tâches" }} />
+            <Tabs.Screen name="ranking" options={{ title: "Classement" }} />
+            <Tabs.Screen name="household" options={{ title: "Foyer" }} />
+        </Tabs>
+    );
 }
