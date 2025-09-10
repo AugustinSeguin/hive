6e19a49e572eb4c0fcd0742f171abaa7db5106b9
AugustinSeguin <augustinseguin@gmail.com>
2025-09-10 16:56:02 +0200

    buttoncomponent

---
+++ components/ButtonComponent.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
+import Sizes from "../constants/Sizes";
+
+type ButtonProps = {
+  type: "primary" | "secondary";
+  action: () => void;
+  titre: string;
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: Sizes.BUTTON_RADIUS,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: Sizes.FONT_SIZE_MD,
+    fontWeight: "bold",
+  },
+});
+
+function getButtonStyles(
+  type: "primary" | "secondary",
+  colorScheme: "light" | "dark" | null | undefined
+) {
+  if (type === "primary") {
+    return {
+      button: {
+        ...styles.button,
+        paddingVertical: Sizes.SPACING_MD,
+        paddingHorizontal: Sizes.SPACING_MD,
+        marginHorizontal: Sizes.SPACING_LG,
+        backgroundColor: Colors[colorScheme ?? "light"].background,
+      },
+      text: {
+        ...styles.text,
+        color: Colors[colorScheme ?? "light"].text,
+      },
+    };
+  } else {
+    const size = Sizes.AVATAR_SIZE_LG;
+    return {
+      button: {
+        ...styles.button,
+        backgroundColor: Colors[colorScheme ?? "light"].background,
+        borderWidth: 1,
+        borderColor: Colors[colorScheme ?? "light"].tint,
+        borderRadius: size / 2,
+        width: size,
+        height: size,
+        justifyContent: "center" as const,
+        alignItems: "center" as const,
+      },
+      text: {
+        ...styles.text,
+        color: Colors[colorScheme ?? "light"].tint,
+        fontSize: Sizes.FONT_SIZE_SM,
+      },
+    };
+  }
+}
+
+const ButtonComponent: React.FC<ButtonProps> = ({ action, titre, type }) => {
+  const colorScheme = useColorScheme();
+  const { button, text } = getButtonStyles(type, colorScheme);
+  return (
+    <TouchableOpacity onPress={action} style={button}>
+      <Text style={text}>{titre}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonComponent;
