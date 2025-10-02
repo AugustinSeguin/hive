b62d380b43d210d2a0a05d7abeeeb0eda254cd95
AugustinSeguin <augustinseguin@gmail.com>
2025-10-02 11:19:19 +0200

    safearea view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -12,18 +13,23 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="register" options={{ headerShown: false }} />
-        <Stack.Screen name="addTask" options={{ title: 'Nouvelle tâche' }} />
-        <Stack.Screen name="settings" options={{ title: 'Paramètres' }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+    return (
+        <SafeAreaProvider>
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                <Stack>
+                    <Stack.Screen name="login" options={{ headerShown: false }} />
+                    <Stack.Screen name="register" options={{ headerShown: false }} />
 
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+                    <Stack.Screen name="addTask" options={{ title: "Nouvelle tâche", headerShown: true }} />
+
+                    <Stack.Screen name="settings" options={{ title: "Paramètres" }} />
+
+                    <Stack.Screen name="+not-found" />
+                </Stack>
+                <StatusBar style="auto" />
+            </ThemeProvider>
+        </SafeAreaProvider>
+    );
+}
